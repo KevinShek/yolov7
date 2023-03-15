eb0129194658d6f2b9770ebbf6f0047f707243ca
Kevin Shek <73186520+KevinShek@users.noreply.github.com>
2023-03-15 11:19:53 +0000

    update for npu

--- det/detect.py
+++ det/detect.py
@@ -75,6 +75,7 @@ def run(
         hide_conf=False,  # hide confidences
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
+        npu=False, # use Vim3Pro NPU for ONNX inference 
 ):
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
@@ -245,6 +246,7 @@ def parse_opt():
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
+    parser.add_argument('--npu', action='store_true', help='use Vim3Pro NPU for ONNX inference')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
